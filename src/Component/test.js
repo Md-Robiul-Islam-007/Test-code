1d81bc105d23c2776257b9ffe591efb6a49809aa
Robi <rjrobi900@gmail.com>
2020-06-18 01:53:12 +0600

    div tag removed

--- src/Component/test.js
+++ src/Component/test.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class Test extends Component {
     render() {
         return (
-            <div>
                 <h2>This is also a test massage</h2>
-            </div>
         );
     }
 }
